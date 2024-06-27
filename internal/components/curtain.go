7d791ccab11f18451beb8cb12d0c93ed7077b333
R S <rheinsnyman@gmail.com>
2024-06-27 19:01:58 +0200

    curtain update

--- internal/components/curtain.go
+++ internal/components/curtain.go
@@ -9,6 +9,7 @@ import (
 
 type curtain struct {
 	img       *ebiten.Image
+	rImg      *ebiten.Image
 	x         float64
 	y         float64
 	direction direction
@@ -38,8 +39,14 @@ func NewCurtain(direction direction) Component {
 		panic("invalid direction")
 	}
 
+	rImg, _, err := ebitenutil.NewImageFromFile("./assets/images/Stall/curtain_rope.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &curtain{
 		img:       img,
+		rImg:      rImg,
 		x:         float64(img.Bounds().Dx()),
 		y:         float64(img.Bounds().Dy()),
 		direction: direction,
@@ -51,6 +58,9 @@ func (curtain *curtain) Draw(screen *ebiten.Image) error {
 	direction := curtain.direction
 	screenX := float64(screen.Bounds().Dx())
 	options := &ebiten.DrawImageOptions{}
+	rOpt := &ebiten.DrawImageOptions{}
+	rY := curtain.img.Bounds().Dy()/2 - curtain.rImg.Bounds().Dy()/2
+	rX := curtain.rImg.Bounds().Dx() / 2
 
 	switch direction {
 	case north:
@@ -63,11 +73,19 @@ func (curtain *curtain) Draw(screen *ebiten.Image) error {
 	case east:
 		options.GeoM.Scale(-1, 1)
 		options.GeoM.Translate(screenX, defaultY)
+		screen.DrawImage(curtain.img, options)
+
+		rOpt.GeoM.Translate(screenX-float64(rX), defaultY+float64(rY))
+		screen.DrawImage(curtain.rImg, rOpt)
+
 	case west:
 		options.GeoM.Translate(0, defaultY)
-	}
+		screen.DrawImage(curtain.img, options)
 
-	screen.DrawImage(curtain.img, options)
+		rOpt = options
+		rOpt.GeoM.Translate(0-float64(rX), float64(rY))
+		screen.DrawImage(curtain.rImg, rOpt)
+	}
 
 	return nil
 }
